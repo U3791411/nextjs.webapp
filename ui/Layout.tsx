6afcca934f397f5524cc5f4bf730e2c272046d27
U1371411 <softwaredeveloper.project@outlook.com>
2023-01-27 09:46:42 +0000

    Build: Global Layout w/ HTML5 Responsive Meta

---
+++ ui/Layout.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>Next.js WebApp</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="description" content="Front-End Software Development w/ Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </>
+  );
+}
