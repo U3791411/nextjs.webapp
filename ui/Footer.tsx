d21057aebf4e29b6f36dba711ae280b655b8feaf
U1371411 <softwaredeveloper.project@outlook.com>
2023-01-29 04:58:13 +0000

    Refactor: Removed Spaces

--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -5,20 +5,12 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-
         <span className="logo">
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width={72}
-            height={16}
-          />
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
-
         <small className="d-block mt-2">
           Copyright &copy; 2023 SOFTW∆REDEV
         </small>
-
       </footer>
     </>
   );
