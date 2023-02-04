d2eb3243db616cf9186e2462a4ac3b16e80319d3
U3791411 <softwaredeveloper.project@outlook.com>
2023-02-04 05:28:42 +0000

    Build: Display Blog Posts

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import Layout, { meta } from '../ui/Layout';
+import Date from '../ui/Date';
+import { getSortedByDateData } from '../lib/api';
 
 type Variable = {
   tag: string;
@@ -11,7 +14,13 @@ export const variable: Variable = {
   pagetitle: 'FE∆TURED BLOGS',
 };
 
-function BlogsPage() {
+function BlogsPage({ markdownData }: {
+  markdownData: {
+    slug: string
+    title: string
+    date: string
+  }[]
+}) {
   return (
     <Layout>
       <Head>
@@ -21,6 +30,16 @@ function BlogsPage() {
         <div className="m-3">
           <small className="fw-bold">{variable.tag}</small>
           <h1 className="mb-4">{variable.pagetitle}</h1>
+          <ul>
+            {markdownData.map(({ slug, title, date }) => (
+              <li className="mb-2 pb-3" key={slug}>
+                <h6 className="mb-0">{title}</h6>
+                <small>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </Layout>
@@ -28,3 +47,13 @@ function BlogsPage() {
 }
 
 export default BlogsPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const markdownData = getSortedByDateData();
+
+  return {
+    props: {
+      markdownData
+    }
+  };
+}
